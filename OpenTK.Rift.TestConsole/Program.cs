4902b55b9215a3b0afa5da019fdc09c6e439defa
thefiddler <stapostol@gmail.com>
2014-02-28 15:19:02 +0100

    Cleaned up console printing

--- OpenTK.Rift.TestConsole/Program.cs
+++ OpenTK.Rift.TestConsole/Program.cs
@@ -8,32 +8,36 @@ namespace OpenTK.Rift.TestConsole
         {
             try
             {
-                OculusRift ovr = new OculusRift();
-
-                do
+                using (OculusRift rift = new OculusRift())
                 {
-                    int left = Console.CursorLeft;
-                    int top = Console.CursorTop;
+                    do
+                    {
+                        int left = Console.CursorLeft;
+                        int top = Console.CursorTop;
 
-                    Console.WriteLine("Orientation (quaternion):\t\t{0}", ovr.Orientation);
-                    Console.WriteLine("Orientation (axis-angle):\t\t{0}", ovr.Orientation.ToAxisAngle());
-                    Console.WriteLine("Acceleration (m/s^2):\t\t{0}", ovr.Acceleration);
-                    Console.WriteLine("Angular velocity (rad/s):\t\t{0}", ovr.AngularVelocity);
-                    Console.WriteLine("Chroma aberration:\t\t{0}", ovr.ChromaAbAberration);
-                    Console.WriteLine("Distortion K:\t\t{0}", ovr.DistortionK);
-                    Console.WriteLine("Eye-screen distance (m):\t\t{0}", ovr.EyeToScreenDistance);
-                    Console.WriteLine("Interpupillary distance (m):\t\t{0}", ovr.InterpupillaryDistance);
-                    Console.WriteLine("Lens separation distance (m):\t\t{0}", ovr.LensSeparationDistance);
-                    Console.WriteLine("Horizonal resolution (px):\t\t{0}", ovr.HResolution);
-                    Console.WriteLine("Vertical resolution (px):\t\t{0}", ovr.VResolution);
-                    Console.WriteLine("Horizontal size (mm):\t\t{0}", ovr.HScreenSize);
-                    Console.WriteLine("Vertical size (mm):\t\t{0}", ovr.VScreenSize);
-                    Console.WriteLine("Vertical center (mm):\t\t{0}", ovr.VScreenCenter);
+                        Console.WriteLine("Orientation (quaternion):     {0}", ovr.Orientation);
+                        Console.WriteLine("Orientation (axis-angle):     {0}", ovr.Orientation.ToAxisAngle());
+                        Console.WriteLine("Acceleration (m/s^2):         {0}", ovr.Acceleration);
+                        Console.WriteLine("Angular velocity (rad/s):     {0}", ovr.AngularVelocity);
+                        Console.WriteLine("Chroma aberration:            {0}", ovr.ChromaAbAberration);
+                        Console.WriteLine("Distortion K:                 {0}", ovr.DistortionK);
+                        Console.WriteLine("Eye-screen distance (m):      {0}", ovr.EyeToScreenDistance);
+                        Console.WriteLine("Interpupillary distance (m):  {0}", ovr.InterpupillaryDistance);
+                        Console.WriteLine("Lens separation distance (m): {0}", ovr.LensSeparationDistance);
+                        Console.WriteLine("Horizonal resolution (px):    {0}", ovr.HResolution);
+                        Console.WriteLine("Vertical resolution (px):     {0}", ovr.VResolution);
+                        Console.WriteLine("Horizontal size (m):          {0}", ovr.HScreenSize);
+                        Console.WriteLine("Vertical size (m):            {0}", ovr.VScreenSize);
+                        Console.WriteLine("Vertical center (m):          {0}", ovr.VScreenCenter);
 
-                    Console.WriteLine("Press any key to exit . . .");
-                    Console.SetCursorPosition(left, top);
+                        Console.WriteLine("Press any key to exit . . .");
+                        Console.SetCursorPosition(left, top);
+                    }
+                    while (!Console.KeyAvailable);
+
+                    for (int i = 0; i < 16; i++)
+                        Console.WriteLine();
                 }
-                while (!Console.KeyAvailable);
             }
             catch (Exception e)
             {
