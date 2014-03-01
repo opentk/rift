9719744bbd8156bc4e42f3060b9c0211baffc98c
thefiddler <stapostol@gmail.com>
2014-03-01 12:15:59 +0100

    Update OculusCamera from Labyrinth project

--- OpenTK.Rift.Test/OculusCamera.cs
+++ OpenTK.Rift.Test/OculusCamera.cs
@@ -28,16 +28,223 @@
 #endregion
 
 using System;
+using OpenTK;
 
 namespace OpenTK.Rift.Test
 {
-    class OculusCamera
+    enum CameraType
     {
-        public OculusCamera()
+        Center = 0,
+        Default = Center, 
+        Left,
+        Right
+    }
+
+    sealed class OculusCamera
+    {
+        readonly OculusRift rift;
+        Matrix4 mono_frustum = Matrix4.Identity;
+        Matrix4 left_frustum = Matrix4.Identity;
+        Matrix4 right_frustum = Matrix4.Identity;
+        Matrix4 left_modelview = Matrix4.Identity;
+        Matrix4 right_modelview = Matrix4.Identity;
+        float ZNear = 0.3f;
+        float ZFar = 1000.0f;
+
+        #region Constructors
+
+        public OculusCamera(OculusRift rift)
+            : this(rift, Vector3.Zero, Quaternion.Identity)
+        { }
+
+        public OculusCamera(OculusRift rift, Vector3 position, Quaternion orientation)
         {
+            if (rift == null)
+                throw new ArgumentNullException();
+            this.rift = rift;
+
+            Position = position;
+            Orientation = orientation;
+
+            EventHandler<EventArgs> update_frustum = (sender, e) =>
+            {
+                float view_center = rift.HScreenSize * 0.25f;
+                float eye_projection_shift = view_center - rift.LensSeparationDistance * 0.5f;
+                float projection_center_offset = 4.0f * eye_projection_shift / rift.HScreenSize;
+
+                GetProjectionMatrix(out mono_frustum);
+                left_frustum = Matrix4.CreateTranslation(projection_center_offset * 0.5f, 0, 0) * mono_frustum;
+                right_frustum = Matrix4.CreateTranslation(-projection_center_offset * 0.5f, 0, 0) * mono_frustum;
+
+                float half_ipd = rift.InterpupillaryDistance * 0.5f;
+                if (view_center != 0)
+                {
+                    left_modelview = Matrix4.CreateTranslation(half_ipd, 0, 0) * view_center;
+                    right_modelview = Matrix4.CreateTranslation(-half_ipd, 0, 0) * view_center;
+                }
+            };
+
+            //FieldOfViewChanged += update_frustum;
+            //AspectRatioChanged += update_frustum;
+            //ZNearChanged += update_frustum;
+            FusionDistanceChanged += update_frustum;
+            InterocularDistanceChanged += update_frustum;
+
+            update_frustum(this, EventArgs.Empty);
         }
 
+        #endregion
 
+        #region Public Members
+
+        #region Events
+
+        public event EventHandler<EventArgs> InterocularDistanceChanged = delegate { };
+        public event EventHandler<EventArgs> FusionDistanceChanged = delegate { };
+
+        #endregion
+
+        #region Properties
+
+        public Vector3 Position { get; set; }
+        public Vector3 TargetPosition { get; set; }
+        public Quaternion Orientation { get; set; }
+        public Quaternion TargetOrientation { get; set; }
+
+        public float AspectRatio
+        {
+            get
+            {
+                return 0.5f * rift.HResolution / (float)rift.VResolution;
+            }
+        }
+
+        public float FieldOfView
+        {
+            get
+            {
+                return (float)(
+                    2.0f * Math.Atan(
+                        0.5f * rift.VScreenSize / rift.EyeToScreenDistance));
+            }
+        }
+
+        public float InterocularDistance
+        {
+            get { return rift.InterpupillaryDistance; }
+            #if false
+            set
+            {
+                if (value < 0)
+                    interocular_distance = 0;
+                interocular_distance = value;
+
+                InterocularDistanceChanged(this, EventArgs.Empty);
+            }
+            #endif
+        }
+
+        #if false
+        // Not necessary for the Oculus Rift
+        float FusionDistance
+        {
+            get { return fusion_distance; }
+            set
+            {
+                if (value < ZNear)
+                    fusion_distance = ZNear;
+                fusion_distance = value;
+
+                FusionDistanceChanged(this, EventArgs.Empty);
+            }
+        }
+        #endif
+
+        #endregion
+
+        #region Methods
+
+        void GetProjectionMatrix(out Matrix4 matrix)
+        {
+            matrix = Matrix4.CreatePerspectiveFieldOfView(
+                (float)(FieldOfView * Math.PI / 180.0),
+                AspectRatio,
+                ZNear,
+                ZFar);
+        }
+
+        void GetModelviewMatrix(out Matrix4 matrix)
+        {
+            var translation_matrix = Matrix4.CreateTranslation(-Position);
+            var rotation_matrix = Matrix4.CreateFromQuaternion(Orientation);
+            Matrix4.Mult(ref translation_matrix, ref rotation_matrix, out matrix);
+        }
+
+        void GetRotationMatrix(out Matrix4 matrix)
+        {
+            matrix = Matrix4.CreateFromQuaternion(Orientation);
+        }
+
+        public void GetProjectionMatrix(CameraType type, out Matrix4 matrix)
+        {
+            switch (type)
+            {
+                case CameraType.Center:
+                    matrix = mono_frustum;
+                    break;
+
+                case CameraType.Left:
+                    matrix = left_frustum;
+                    break;
+
+                case CameraType.Right:
+                    matrix = right_frustum;
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public void GetModelviewMatrix(CameraType type, out Matrix4 matrix)
+        {
+            this.GetModelviewMatrix(out matrix);
+
+            switch (type)
+            {
+                case CameraType.Center:
+                    // Nothing to do
+                    break;
+
+                case CameraType.Left:
+                    Matrix4.Mult(ref matrix, ref left_modelview, out matrix);
+                    break;
+
+                case CameraType.Right:
+                    Matrix4.Mult(ref matrix, ref right_modelview, out matrix);
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public void GetModelviewProjectionMatrix(CameraType type, out Matrix4 matrix)
+        {
+            Matrix4 modelview;
+            GetModelviewMatrix(type, out modelview);
+            GetProjectionMatrix(type, out matrix);
+            Matrix4.Mult(ref modelview, ref matrix, out matrix);
+        }
+
+        public void GetRotationMatrix(CameraType type, out Matrix4 matrix)
+        {
+            this.GetRotationMatrix(out matrix);
+        }
+
+        #endregion
+
+        #endregion
     }
 }
 
