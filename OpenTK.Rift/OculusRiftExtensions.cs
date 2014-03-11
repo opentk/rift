4228fe75e11664bb9b2988f8843515880de35edf
thefiddler <stapostol@gmail.com>
2014-03-11 01:57:53 +0100

    Added GetDisplay extension method. Documented all extension methods.

--- OpenTK.Rift/OculusRiftExtensions.cs
+++ OpenTK.Rift/OculusRiftExtensions.cs
@@ -9,6 +9,47 @@ namespace OpenTK
     {
         #region Public Members
 
+        /// <summary>
+        /// Gets the <see cref="OpenTK.DisplayDevice"/> that
+        /// corresponds to the specified <see cref="OpenTK.OculusRift"/>
+        /// device.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="OpenTK.DisplayDevice"/> that corresponds
+        /// to an Oculus Rift, or <see cref="OpenTK.DisplayDevice.Default"/>
+        /// if no Oculus Rift device is connected.
+        /// </returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        public static DisplayDevice GetDisplay(this OculusRift rift)
+        {
+            if (rift == null)
+                throw new ArgumentNullException();
+
+            if (rift.IsConnected)
+            {
+                bool is_rift = false;
+                for (var i = DisplayIndex.First; i < DisplayIndex.Sixth; i++)
+                {
+                    var display = DisplayDevice.GetDisplay(i);
+                    is_rift = display != null;
+                    is_rift &= display.Width == rift.HResolution;
+                    is_rift &= display.Height == rift.VResolution;
+                    if (is_rift)
+                    {
+                        return display;
+                    }
+                }
+            }
+            return DisplayDevice.Default;
+        }
+
+        /// <summary>
+        /// Gets the field of view of the specified <see cref="OpenTK.OculusRift"/>, in radians.
+        /// </summary>
+        /// <returns>
+        /// The field of view of an <see cref="OpenTK.OculusRift"/>, in radians.
+        /// </returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
         public static float GetFieldOfView(this OculusRift rift)
         {
             if (rift == null)
@@ -19,6 +60,12 @@ namespace OpenTK
             return 2.0f * (float)Math.Atan(0.5f * rift.VScreenSize / rift.EyeToScreenDistance);
         }
 
+        /// <summary>
+        /// Gets the aspect ratio of the specified <see cref="OpenTK.OculusRift"/>,
+        /// for a single eye.
+        /// </summary>
+        /// <returns>The aspect ratio of an <see cref="OpenTK.OculusRift"/>.</returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
         public static float GetAspectRatio(this OculusRift rift)
         {
             if (rift == null)
@@ -29,6 +76,15 @@ namespace OpenTK
             return 0.5f * rift.HResolution / (float)rift.VResolution;
         }
 
+        /// <summary>
+        /// Gets the left projection matrix for the specified <see cref="OpenTK.OculusRift"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="OpenTK.Matrix4"/> representing the projection matrix for the left eye.
+        /// </returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        /// <param name="near">The near clipping plane of the view frustum.</param>
+        /// <param name="far">The far clipping plane of the view frustum.</param>
         public static Matrix4 GetLeftProjection(this OculusRift rift, float near, float far)
         {
             Matrix4 projection;
@@ -36,12 +92,30 @@ namespace OpenTK
             return projection;
         }
 
+        /// <summary>
+        /// Gets the left projection matrix for the specified <see cref="OpenTK.OculusRift"/>.
+        /// </summary>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        /// <param name="near">The near clipping plane of the view frustum.</param>
+        /// <param name="far">FThe far clipping plane of the view frustumar.</param>
+        /// <param name="projection">
+        /// A <see cref="OpenTK.Matrix4"/> representing the projection matrix for the left eye.
+        /// </param>
         public static void GetLeftProjection(this OculusRift rift, float near, float far,
             out Matrix4 projection)
         {
             GetProjection(rift, near, far, 1, out projection);
         }
 
+        /// <summary>
+        /// Gets the right projection matrix for the specified <see cref="OpenTK.OculusRift"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="OpenTK.Matrix4"/> representing the projection matrix for the right eye.
+        /// </returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        /// <param name="near">The near clipping plane of the view frustum.</param>
+        /// <param name="far">The far clipping plane of the view frustum.</param>
         public static Matrix4 GetRightProjection(this OculusRift rift, float near, float far)
         {
             Matrix4 projection;
@@ -49,36 +123,73 @@ namespace OpenTK
             return projection;
         }
 
+        /// <summary>
+        /// Gets the right projection matrix for the specified <see cref="OpenTK.OculusRift"/>.
+        /// </summary>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        /// <param name="near">The near clipping plane of the view frustum.</param>
+        /// <param name="far">The far clipping plane of the view frustum.</param>
+        /// <param name="projection">
+        /// A <see cref="OpenTK.Matrix4"/> representing the projection matrix for the right eye.
+        /// </param>
         public static void GetRightProjection(this OculusRift rift, float near, float far,
             out Matrix4 projection)
         {
             GetProjection(rift, near, far, -1, out projection);
         }
 
-        public static Matrix4 GetLeftModelview(this OculusRift rift)
+        /// <summary>
+        /// Gets the left translation matrix for the specified <see cref="OpenTK.OculusRift"/> instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="OpenTK.Matrix4"/> representing the translation matrix for the left eye.
+        /// </returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        public static Matrix4 GetLeftTranslation(this OculusRift rift)
         {
             Matrix4 modelview;
-            rift.GetLeftModelview(out modelview);
+            rift.GetLeftTranslation(out modelview);
             return modelview;
         }
 
-        public static void GetLeftModelview(this OculusRift rift,
+        /// <summary>
+        /// Gets the left translation matrix for the specified <see cref="OpenTK.OculusRift"/> instance.
+        /// </summary>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        /// <param name="modelview">
+        /// A <see cref="OpenTK.Matrix4"/> representing the translation matrix for the left eye.
+        /// </param>
+        public static void GetLeftTranslation(this OculusRift rift,
             out Matrix4 modelview)
         {
-            GetModelview(rift, 1, out modelview);
+            GetTranslation(rift, 1, out modelview);
         }
 
-        public static void GetRightModelview(this OculusRift rift,
-            out Matrix4 modelview)
-        {
-            GetModelview(rift, -1, out modelview);
-        }
-
-        public static Matrix4 GetRightModelview(this OculusRift rift)
+        /// <summary>
+        /// Gets the right translation matrix for the specified <see cref="OpenTK.OculusRift"/> instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="OpenTK.Matrix4"/> representing the translation matrix for the right eye.
+        /// </returns>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        public static Matrix4 GetRightTranslation(this OculusRift rift)
         {
             Matrix4 modelview;
-            rift.GetRightModelview(out modelview);
+            rift.GetRightTranslation(out modelview);
             return modelview;
+        }
+
+        /// <summary>
+        /// Gets the right translation matrix for the specified <see cref="OpenTK.OculusRift"/> instance.
+        /// </summary>
+        /// <param name="rift">An <see cref="OpenTK.OculusRift"/> instance.</param>
+        /// <param name="modelview">
+        /// A <see cref="OpenTK.Matrix4"/> representing the translation matrix for the right eye.
+        /// </param>
+        public static void GetRightTranslation(this OculusRift rift,
+            out Matrix4 modelview)
+        {
+            GetTranslation(rift, -1, out modelview);
         }
 
         #endregion
@@ -108,12 +219,12 @@ namespace OpenTK
                 Matrix4.CreateTranslation(
                     projection_center_offset, 0, 0,
                     out translation);
-                
+
                 Matrix4.Mult(ref projection, ref translation, out projection);
             }
         }
 
-        static void GetModelview(OculusRift rift, float eye,
+        static void GetTranslation(OculusRift rift, float eye,
             out Matrix4 modelview)
         {
             float view_center = rift.HScreenSize * 0.25f;
